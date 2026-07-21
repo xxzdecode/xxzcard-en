71ce3663f20caf965e1f58096f650c1360b0b721
xxz de code <113927546+xxzdecode@users.noreply.github.com>
2026-07-21 21:39:20 +0800

    fix: correct July 22 grammar challenge title

--- grammar-challenge/data/catalog.js
+++ grammar-challenge/data/catalog.js
@@ -2,9 +2,9 @@
   // 新增练习时：数据型练习使用 dataPath；复用随堂练习 UI 的独立页面使用 pagePath。
   window.GRAMMAR_CHALLENGE_CATALOG = [
     {
-      id: 'grammar-2026-07-22-simple-present-2',
+      id: 'grammar-2026-07-22-simple-present-1',
       date: '2026-07-22',
-      title: '一般现在时第二课｜否定与提问',
+      title: '一般现在时第一课｜三单',
       pagePath: './practices/2026-07-22.html'
     },
     {
