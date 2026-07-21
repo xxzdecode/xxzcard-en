8eb80e8ccb328c171c194462f9292635582da969
xxzdecode <stella_chenxin@outlook.com>
2026-07-22 01:30:01 +0800

    feat: publish special questions lesson exercises

--- grammar-challenge/data/catalog.js
+++ grammar-challenge/data/catalog.js
@@ -2,6 +2,12 @@
   // 新增练习时：数据型练习使用 dataPath；复用随堂练习 UI 的独立页面使用 pagePath。
   window.GRAMMAR_CHALLENGE_CATALOG = [
     {
+      id: 'grammar-2026-07-23-simple-present-2',
+      date: '2026-07-23',
+      title: '一般现在时第二课｜语法挑战',
+      pagePath: './practices/2026-07-23.html'
+    },
+    {
       id: 'grammar-2026-07-22-simple-present-1',
       date: '2026-07-22',
       title: '一般现在时第一课｜三单',
