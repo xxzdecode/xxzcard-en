75841b453eca38e964255a32a3176d7c4748fbc8
xxz de code <113927546+xxzdecode@users.noreply.github.com>
2026-07-30 14:05:01 +0800

    Refresh app shell for copy-list export

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_SHELL_CACHE = 'xxzcard-app-shell-v32';
-const RUNTIME_CACHE = 'xxzcard-runtime-v32';
+const APP_SHELL_CACHE = 'xxzcard-app-shell-v33';
+const RUNTIME_CACHE = 'xxzcard-runtime-v33';
 const CACHE_PREFIXES = ['xxzcard-', 'vocabulary-review-'];
 const APP_SHELL = [
   './index.html',
